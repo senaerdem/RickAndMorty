e40a951aa2e06981307ded4595c9abd0c3f15d21
senaerdem <senanurerdem07@gmail.com>
2024-04-18 22:48:59 +0300

    Initial commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Sena Nur Erdem on 18.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
