bf41679606bffea3e438eb2f37e7da07b0fd1cd0
senaerdem <senanurerdem07@gmail.com>
2024-04-19 20:52:51 +0300

    API request created
    Created URLs to represent API calls and based on endpoints, path components, and query parameters.

--- RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/APIClient/RMService.swift
@@ -16,10 +16,15 @@ final class RMService {
     private init() {}
     
     // Send Rick and Morty API Call
-    // - Parameters:
-    //   request : Request instance
-    //   completion : Callback with data or error
-    public func execute(_ request: RMRequest, completion: @escaping () -> Void) {
-        
+    //  Parameters:
+    //  - request : Request instance
+    //  - type : The type of object we expected to get back
+    //  - completion : Callback with data or error
+    public func execute<T: Codable>(
+        _ request: RMRequest,
+        expecting type: T.Type,
+        completion: @escaping (Result<String, Error>
+        ) -> Void
+    ) {
     }
 }
