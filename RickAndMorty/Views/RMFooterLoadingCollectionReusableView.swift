06c822bafd9d18c874562043b6459ed9bf21c427
senaerdem <senanurerdem07@gmail.com>
2024-04-24 23:15:01 +0300

    RMFooterLoadingCollectionReusableView created

---
+++ RickAndMorty/Views/RMFooterLoadingCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  RMFooterLoadingCollectionReusableView.swift
+//  RickAndMorty
+//
+//  Created by Sena Nur Erdem on 24.04.2024.
+//
+
+import UIKit
+
+final class RMFooterLoadingCollectionReusableView: UICollectionReusableView {
+        
+}
