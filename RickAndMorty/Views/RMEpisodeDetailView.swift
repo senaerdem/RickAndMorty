96c48937f1a99fb45339c1b7e6156718aa7c61db
senaerdem <senanurerdem07@gmail.com>
2024-05-12 17:04:36 +0300

    added episode detail view

---
+++ RickAndMorty/Views/RMEpisodeDetailView.swift
@@ -0,0 +1,17 @@
+//
+//  RMEpisodeDetailView.swift
+//  RickAndMorty
+//
+//  Created by Sena Nur Erdem on 12.05.2024.
+//
+
+import UIKit
+
+final class RMEpisodeDetailView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("unsupported")
+    }
+}
