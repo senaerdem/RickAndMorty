8b040446d04483d5757919ceed9abeea9a0fbeb4
senaerdem <senanurerdem07@gmail.com>
2024-05-12 17:07:46 +0300

    api cache layer
    Introduces a caching layer that enhances data retrieval by storing API responses in memory, reducing the need for repetitive network requests and speneding up the application's responsiveness.

--- RickAndMorty/ViewModels/RMEpisodeDetailViewViewModel.swift
+++ RickAndMorty/ViewModels/RMEpisodeDetailViewViewModel.swift
@@ -8,9 +8,28 @@
 import UIKit
 
 class RMEpisodeDetailViewViewModel: NSObject {
+    
     private let endpointUrl: URL?
     
-    init(url: URL?) {
-        self.endpointUrl = url
+    init(endpointUrl: URL?) {
+        self.endpointUrl = endpointUrl
+        super.init()  // first super init
+        fetchEpisodeData()
+    }
+    
+    private func fetchEpisodeData() {
+        guard let url = endpointUrl,
+                let request = RMRequest(url: url) else {
+            return
+        }
+        
+        RMService.shared.execute(request, expecting: RMEpisode.self) { result in
+            switch result {
+            case .success(let success):
+                print(String(describing: success))
+            case.failure(let failure):
+                break
+            }
+        }
     }
 }
